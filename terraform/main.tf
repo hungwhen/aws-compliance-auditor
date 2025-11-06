03cfb3c6152c47ccc2821f4b040a50a84bea0601
hungwhen <hungwhen@gmail.com>
2025-11-06 14:06:08 -0600

    Add AWS IAM role and policy for AWS Config

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,3 +36,55 @@ resource "aws_s3_bucket" "config_logs" {
   }
 
 }
+
+resource "aws_iam_role" "aws_config" {
+  name = "aws-config-role-${var.account_id}-${var.region}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "config.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "aws_config" {
+  name = "aws-config-policy"
+  role = aws_iam_role.aws_config.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetBucketAcl",
+          "s3:GetBucketLocation", 
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.config_logs.arn,
+          "${aws_s3_bucket.config_logs.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_config_configuration_recorder" "main" {
+  name = "default"
+  role_arn = aws_iam_role.aws_config.arn
+
+  recording_group {
+    all_supported = true
+    include_global_resource_types = true
+  }
+
+}
