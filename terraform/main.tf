419750c52b8bee915c8fb30073ebbd380fd5571d
hungwhen <hungwhen@gmail.com>
2025-11-26 22:35:22 -0600

    added more stuff to the terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,5 +86,156 @@ resource "aws_config_configuration_recorder" "main" {
     all_supported = true
     include_global_resource_types = true
   }
+}
+
+resource "aws_iam_role_policy_attachment" "aws_config_managed" {
+    role = aws_iam_role.aws_config.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}
+
+resource "aws_s3_bucket_policy" "config_logs" {
+
+    bucket = aws_s3_bucket.config_logs.id
+
+    policy = jsonencode({
+    
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid = "AWSConfigBucketPermissionsCheck"
+                Effect = "Allow"
+                Principal = {
+                    Service = "config.amazonaws.com"
+                }
+                Action = "s3:GetBucketAcl"
+                Resource = aws_s3_bucket.config_logs.arn
+            },
+            {
+                Sid = "AWSConfigBucketDelivery"
+                Effect = "Allow"
+                Principal = {
+                    Service = "config.amazonaws.com"
+                }
+                Action = "s3:PutObject"
+                Resource = "${aws_s3_bucket.config_logs.arn}/*"
+                Condition = {
+                    StringEquals = {
+                        "s3:x-amz-acl" = "bucket-owner-full-control"
+                    }
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_config_delivery_channel" "main" {
+    
+    name = "default"
+    s3_bucket_name = aws_s3_bucket.config_logs.bucket    
+
+    sns_topic_arn = aws_sns_topic.compliance_alerts.arn
+
+    depends_on = [aws_config_configuration_recorder.main]
+
+}
+
+resource "aws_config_configuration_recorder_status" "main" {
+    
+    name = aws_config_configuration_recorder.main.name
+    is_enabled = true
+
+    depends_on = [aws_config_delivery_channel.main]
+
+}
+
+#misconfigs
+
+resource "aws_config_config_rule" "s3_public_read_prohibited" {
+    name = "s3-bucket-public-read-prohibited"
+    source {
+        owner = "AWS"
+        source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
+    }
+    tags = {
+        Frameworks = "CIS-1.2.0, NIST-AC-3, ISO-27001-A.9.1"
+    }
+}
+
+resource "aws_config_config_rule" "s3_public_write_prohibited" {
+
+    name = "s3-bucket-public-write-prohibited"
+    source {
+        owner = "AWS"
+        source_identifier  = "S3_BUCKET_PUBLIC_WRITE_PROHIBITED"
+    }
+    tags = {
+        Frameworks = "CIS-1.2.0, NIST-SC-7, ISO-27001-A.13.1"
+    }
+}
+
+resource "aws_config_config_rule" "restricted_ssh" {
+
+    name = "restricted-ssh"
+
+    source = {
+        owner = "AWS"
+        source_identifier = "INCOMING_SSH_DISABLED"
+    }
+
+    tags = {
+        Frameworks = "CIS-4.1, NIS-AC-4, ISO-27001-A.13.1"
+    }
+}
+
+resource "aws_config_config_rule" "iam_access_keys_rotated" {
+    name = "iam-access-keys-rotated"
+    source {
+        owner = "AWS"
+        source_identifier = "ACCESS_KEYS_ROTATED"
+    }
+    input_parameters = jsonencode({
+        maxAccessKeyAge = "90" # this is in days
+    })
+    tags = {
+        Frameworks = "CIS-1.4,NIST-IA-5,ISO-27001-A.9.2"
+    }
+}
+
+resource "aws_sns_topic" "compliance_alerts" {
+    name = "compliance-alerts"
+}
+
+resource "aws_sns_topic_subscription" "compliance_email" {
+    topic_arn = aws_sns_topic.compliance_alerts.arn
+    protocol = "email"
+    endpoint = "thisisanemailillreplaceingitignoreandafilelater@gmail.com"
+}
+
+resource "awS_s3_bucket_versioning" "config_logs" {
+    bucket = aws_s3_bucket.config_logs.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "config_logs" {
+    bucket = aws_s3_bucket.config_logs.id
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm =  "aws:kms"
+        }
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "config_logs" {
+
+    bucket = aws_s3_bucket.config_logs.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+
+}
+
 
 }
